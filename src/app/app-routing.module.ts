ac9f2174c97044b0e6f5809f8a29db2b73524034
nx-ng-nest <101035559+nx-ng-nest@users.noreply.github.com>
2022-04-08 21:32:38 -0500

    github page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ import {
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true, initialNavigation: true }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
